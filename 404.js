cc5860c3c8aadef5d463128fbdb315998e761ceb
William <william.guimont-martin.1@ulaval.ca>
2018-12-25 19:24:46 -0500

    Simple 404

---
+++ 404.js
@@ -0,0 +1,23 @@
+var parent = document.getElementById('404');
+var x = 0;
+
+function setup()
+{
+    var canvas = createCanvas(parent.offsetWidth, parent.offsetHeight);
+    canvas.parent('404');
+}
+
+function draw()
+{
+    clear();
+    console.log(parent.parent);
+    fill(0);
+    textSize(80);
+    textAlign(CENTER);
+    text("404", (width/2 - 100) * sin(x) + width / 2, 70);
+    x += 0.01;
+}
+
+function windowResized() {
+    resizeCanvas(parent.offsetWidth, parent.offsetHeight);
+  }
